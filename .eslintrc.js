447580c4299c3ddb30fbf6e894c7fb89810fe917
Thiago Medeiros <thihxm@gmail.com>
2019-11-22 15:06:33 -0300

    Configuração para usar import/export

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,22 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
-    'prettier',
+    'airbnb',
   ],
-  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
-    'prettier/prettier': 'error',
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    'camelcase': 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }]
   },
 };
