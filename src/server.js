43ff7cb4834490ea7483b034631497cff35c8c8c
Thiago Medeiros <thihxm@gmail.com>
2019-11-22 15:36:04 -0300

    conexão mongoose

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import routes from './routes';
 
 const server = express();
 
-server.use(routes)
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-qml6v.mongodb.net/tindev?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333);
